1fbd59499c3875855942139d623b1ed7969d7c90
broop <soundwanders@gmail.com>
2021-02-12 22:40:03 -0500

    create object book and function to add book to library. local storage update function skeleton

--- assets/timbuktu.js
+++ assets/timbuktu.js
@@ -1,11 +1,33 @@
-// JavaScript
+// JavaScript Library
 
-const soundwanders = {
-    Oxygen: 65 ,
-    Carbon: 18 ,
-    Hydrogen: 10 ,
-    Nitrogen: 3 ,
-    Calcium: 1.5 ,
-    Phosphorous: 1.5 ,
-    otherStuff: 1
+// timbuktu is the library
+let timbuktu;
+
+// construct an object 'book' with properties for title, author, publishing date, subject, and status
+class book {
+    constructor(title, author, published, subject, status) {
+        this.title = title;
+        this.author = author;
+        this.published = published;
+        this.subject = subject;
+        this.status = status;
+    }
+};
+
+function newBook() {
+    if (title.value.length === 0 || author.value.length === 0 || published.value.length === 0 || subject.value.length === 0) {
+    confirm("Some fields are still empty, add the new book anyway?");
+        if (confirm) { 
+            const newBook = new book(title.value, author.value, published.value, subject.value, status.value);
+            timbuktu.push(newBook);
+            updateLocalStorage();
+            console.log("New book added to library.");
+        } else {
+            return;
+        }
+    }
+};
+
+function updateLocalStorage() {
+    localStorage.setItem(timbuktu);
 }
