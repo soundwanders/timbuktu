f1b3bb9b8400118fc99396e9b2bda34e41ed59e0
broop <soundwanders@gmail.com>
2021-02-15 15:19:55 -0500

    initial setup, functional library. need to program audio/pdf books embedded in page

--- assets/timbuktu.js
+++ assets/timbuktu.js
@@ -3,31 +3,126 @@
 // timbuktu is the library
 let timbuktu;
 
-// construct an object 'book' with properties for title, author, publishing date, subject, and status
-class book {
-    constructor(title, author, published, subject, status) {
+// Default library data
+const DEFAULT_DATA = [
+    { title: "Agricola and Germanica", author: "Tacitus", status: "read" } ,
+        {
+        title: "Hagakure" ,
+        author: "Yamamoto Tsunetomo",
+        status: "read",
+        },
+    { title: "The Kingdom of the Hittites", author: "Trevor Bryce", status: "not read" } ,
+    { title: "Sassanian Persia", author: "Touraj Daryaee", status: "not read" } ,
+];
+
+// Book properties
+const title = document.querySelector("#title");
+const author = document.querySelector("#author");
+const status = document.querySelector("#status");
+const tableBody = document.querySelector("#book-table-body");
+
+// Submit button, adds new book into array
+const form = document.querySelector("form").addEventListener("submit", (e) => {
+  e.preventDefault();
+  addBook();
+  render();
+  clearForm();
+});
+
+// Add event listener to table
+    // Delete selected books from library, prompt user for confirmation to delete
+    // uses 'deleteBook' , 'findBook' and 'render' functions
+const table = document.querySelector("table").addEventListener("click", (e) => {
+    const currentTarget = e.target.parentNode.parentNode.childNodes[1];
+    if (e.target.innerHTML == "delete") {
+        if (confirm(`Are you sure you want to delete ${currentTarget.innerText}`))
+            deleteBook(findBook(timbuktu, currentTarget.innerText));
+            console.log("Deleted book from library");
+        }
+        if (e.target.classList.contains("status-button")) {
+            changeStatus(findBook(timbuktu, currentTarget.innerText));
+        }
+    updateLocalStorage();
+    render();
+});
+
+// Create Object 'book' with arguments for title, author and status
+class Book {
+    constructor(title, author, status) {
         this.title = title;
         this.author = author;
-        this.published = published;
-        this.subject = subject;
         this.status = status;
     }
 };
 
-function newBook() {
-    if (title.value.length === 0 || author.value.length === 0 || published.value.length === 0 || subject.value.length === 0) {
-    confirm("Some fields are still empty, add the new book anyway?");
-        if (confirm) { 
-            const newBook = new book(title.value, author.value, published.value, subject.value, status.value);
-            timbuktu.push(newBook);
-            updateLocalStorage();
-            console.log("New book added to library.");
-        } else {
-            return;
-        }
+// Add new book into the library
+function addBook() {
+    if (title.value.length === 0 || author.value.length === 0) {
+        alert("Please fill all fields");
+        return;
     }
+    const newBook = new Book(title.value, author.value, status.value);
+    timbuktu.push(newBook);
+    updateLocalStorage();
+    console.log("New book has been added to the library.");
 };
 
+// Change status on book, Read or not read
+function changeStatus(book) {
+    if (timbuktu[book].status === "read") {
+        timbuktu[book].status = "not read";
+    } else timbuktu[book].status = "read";
+};
+
+// Splice to delete book from library
+function deleteBook(currentBook) {
+    timbuktu.splice(currentBook, currentBook + 1);
+};
+
+// Loop through array to find book
+function findBook(timbuktuArray, title) {
+    if (timbuktuArray.length === 0 || timbuktuArray === null) {
+    return;
+    }
+    for (book of timbuktuArray)
+        if (book.title === title) {
+            return timbuktuArray.indexOf(book);
+        }
+};
+
+// Clear input form on new book submission
+function clearForm() {
+    title.value = "";
+    author.value = "";
+};
+
+// Local Storage
 function updateLocalStorage() {
-    localStorage.setItem(timbuktu);
+     localStorage.setItem("timbuktu", JSON.stringify(timbuktu));
+};
+
+function checkLocalStorage() {
+    if (localStorage.getItem("timbuktu")) {
+    timbuktu = JSON.parse(localStorage.getItem("timbuktu"));
+    } else timbuktu = DEFAULT_DATA;
+};
+
+function render() {
+  checkLocalStorage();
+  tableBody.innerHTML = "";
+  timbuktu.forEach((book) => {
+    const htmlBook =
+    `
+      <tr>
+        <td>${book.title}</td>
+        <td>${book.author}</td>
+        <td><button class="status-button">${book.status}</button></td>
+        <td><button class="delete">delete</button></td>
+      </tr>
+    `;
+    
+    tableBody.insertAdjacentHTML("afterbegin", htmlBook);
+  });
 }
+
+render();
