5b8ec48d6c7318db724806742e2ade364e337f03
broop <soundwanders@gmail.com>
2021-03-28 01:24:25 -0400

    Retrieved snapshot of data, need to figre out how to load it back into the display

--- assets/timbuktu.js
+++ assets/timbuktu.js
@@ -161,36 +161,38 @@ const autoId = rootRef.push().key;
 const userId = firebase.auth().currentUser;
 
 // FIREBASE REALTIME DATABASE
-  // Delete old data, Create new database entry
-  const saveButton = document.getElementById('saveData').addEventListener('click', (e) => {
-    e.preventDefault();
-    e.stopImmediatePropagation();
+// Delete old data, Create new database entry
+const saveButton = document.getElementById('saveData').addEventListener('click', (e) => {
+  e.preventDefault();
+  e.stopImmediatePropagation();
 
-    // REMOVE old data
-    rootRef.child(userId ?? autoId).remove();
-    // SAVE new data
-    rootRef.child(userId ?? autoId).set({
-      timbuktu: JSON.stringify(timbuktu)
+  // REMOVE old data
+  rootRef.child(autoId).remove();
+  // SAVE new data
+  rootRef.child(autoId).set({
+    timbuktu: JSON.stringify(timbuktu)
+  })
+  console.log('Saved new data to database');
+});
+
+// LOAD database (takes a snapshot of the referenced data, then parse to timbuktu array);
+const getData = document.getElementById('getData').addEventListener('click', (e) => {
+  e.preventDefault;
+  e.stopImmediatePropagation;
+
+  rootRef.once('value', (snapshot) => {
+    snapshot.forEach((childSnapshot) => {
+      const childBooks = childSnapshot.val();
+      const bookData = JSON.stringify(childBooks);
+
+      timbuktu.push(bookData);
+      console.log(childBooks);
+      console.log(bookData);
     })
-    console.log('Saved new data to database');
-  });
+    console.log("Data loaded")
+  })
+});
 
-  // LOAD database (takes a snapshot of the referenced data, then parse to timbuktu array);
-  const getData = document.getElementById('getData').addEventListener('click', (e) => {
-    e.preventDefault;
-    e.stopImmediatePropagation;
-
-    rootRef.once('value') , function (snapshot) {
-      //timbuktu = snapshot.toArray(JSON.parse(timbuktu));
-      timbuktu.forEach(Book => timbuktu.push(Book)) ,
-      console.log('Data loaded', timbuktu);
-    },
-      function (error) {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        console.log('Error, unable to load data from the database', errorCode, errorMessage);
-      }
-  });
 
 // FIREBASE USER AUTHENTICATION
 // Log in with Google
