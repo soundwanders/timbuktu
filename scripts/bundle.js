005b851b3db4affc2fad0f80641c25ddd4dd0863
soundwanders <soundwanders@gmail.com>
2021-05-11 20:08:04 -0400

    tweaking viewport meta for proper mobile display.

--- scripts/bundle.js
+++ scripts/bundle.js
@@ -1,4 +1,4 @@
-// ignore this file
+// ignore
 // not part of timbuktu build
 
 (function () { function r (e, n, t) { function o (i, f) { if (!n[i]) { if (!e[i]) { const c = typeof require === 'function' && require; if (!f && c) return c(i, !0); if (u) return u(i, !0); const a = new Error("Cannot find module '" + i + "'"); throw a.code = 'MODULE_NOT_FOUND', a; } const p = n[i] = { exports: {} }; e[i][0].call(p.exports, function (r) { const n = e[i][1][r]; return o(n || r); }, p, p.exports, r, e, n, t); } return n[i].exports; } for (var u = typeof require === 'function' && require, i = 0; i < t.length; i++)o(t[i]); return o; } return r; })()({
